16ceadd5f155c2a362c9dc8df5b08dfd61264c69
LKBbb <shs7556@naver.com>
2022-05-23 17:11:50 +0900

    220523 깃허브 최초 커밋

---
+++ day1/test.py
@@ -0,0 +1,29 @@
+from dataclasses import asdict
+
+
+print('Hello, Python!')
+a = 'Hello, Python.'
+print(a)
+a = 10
+print(a)
+a = False
+print(a)
+#주석
+#변수명 지정, 영문자, _로시작, 특수문자는 _만 사용.
+var1 = 17
+Var1 = 19
+바20 = 20
+print(var1)
+print(Var1)
+print(바20)
+#4var = 4
+#5_var = 5
+var_5 = 5
+#var$7 = 7
+_var_8 = 8
+var_8_ = 8.8
+#var!~56
+#var 10 = 10
+#RemainOfBankAccount 은행계좌 잔고
+print(var1 == Var1)
+
