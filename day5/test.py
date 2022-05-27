f9b50a2bf17e21b91d209da7b28098102a3c6c0e
LKBbb <shs7556@naver.com>
2022-05-27 12:17:34 +0900

    220527 파이썬 5일차 오전

--- day5/test.py
+++ day5/test.py
@@ -10,5 +10,5 @@ BASE_DIR = os.path.dirname(PRJ_ROOT)
 print(BASE_DIR) # 프로젝트 기반 폴더 경로
 
 f = open('./test_console.txt', mode = 'w', encoding = 'UTF-8')
-f.write('Hello, World\n')
+f.write('Hello, World !\n')
 f.close()
